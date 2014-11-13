e4e05fd68c78c585f5da38f10084530a93967b54
Androlo <andreas@erisindustries.com>
2014-11-12 22:52:33 -0500

    Parse script

--- core/core.go
+++ core/core.go
@@ -32,11 +32,27 @@ type FileIO interface {
 	WriteFile(directory, name string, data []byte) error
 }
 
+type FileIO interface {
+	Root() string
+	Log() string
+	Apps() string
+	Blockchains() string
+	Filesystems() string
+	Modules() string
+	// Useful when you want to load a file inside of a directory gotten by the 
+	// 'Paths' object. Reads and returns the bytes.
+	ReadFile(directory, name string) ([]byte, error)
+	// Useful when you want to save a file into a directory gotten by the 'Paths'
+	// object.
+	WriteFile(directory, name string, data []byte) error
+}
+
 type Runtime interface {
 	BindScriptObject(name string, val interface{}) error
 	LoadScriptFile(fileName string) error
 	LoadScriptFiles(fileName ...string) error
 	AddScript(script string) error
+	ParseScript(script string) error
 	RunAction(path []string, actionName string, params interface{}) ([]string, error)
 	CallFuncOnObj(objName, funcName string, params ...interface{})
 	RunFunction(funcName string, params interface{}) ([]string, error)
