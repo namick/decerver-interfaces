2c72dc6c90ccde94acb08e38db6223317e7fb14f
Ethan Buchman <ethan@coinculture.info>
2014-12-07 02:14:27 -0500

    decerver dir structure and named chains

--- glue/utils/utils.go
+++ glue/utils/utils.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	"os/user"
 	"path"
-	_ "strconv"
+    "bytes"
+    "io/ioutil"
+    "encoding/json"
 
 	"github.com/eris-ltd/thelonious/monkcrypto"
 	"github.com/eris-ltd/thelonious/monkdb"
@@ -19,6 +21,15 @@ import (
 var (
 	GoPath = os.Getenv("GOPATH")
 	usr, _ = user.Current() // error?!
+    ErisLtd = path.Join(GoPath, "src", "github.com", "eris-ltd")
+
+    Decerver = path.Join(usr.HomeDir, ".decerver")
+    Apps = path.Join(Decerver, "apps")    
+    Blockchains = path.Join(Decerver, "blockchains")    
+    Filesystems = path.Join(Decerver, "filesystems")    
+    Logs = path.Join(Decerver, "logs")
+    Modules = path.Join(Decerver, "modules")
+    Scratch = path.Join(Decerver, "scratch")
 )
 
 func NewDatabase(dbName string) monkutil.Database {
@@ -120,12 +131,65 @@ func Copy(src, dst string) error {
 	return nil
 }
 
-func InitDataDir(Datadir string) {
+func InitDataDir(Datadir string) error {
 	_, err := os.Stat(Datadir)
 	if err != nil {
 		if os.IsNotExist(err) {
-			fmt.Printf("Data directory '%s' doesn't exist, creating it\n", Datadir)
-			os.Mkdir(Datadir, 0777)
+			fmt.Printf("Creating directory: '%s'\n", Datadir)
+			err := os.MkdirAll(Datadir, 0777)
+            if err != nil{
+                return err
+            }
 		}
 	}
+    return nil
 }
+
+func InitDecerverDir() error{
+    dirs := []string{Decerver, Apps, Blockchains, Filesystems, Logs, Modules, Scratch}
+    for _, d := range dirs{
+        err := InitDataDir(d)
+        if err != nil{
+            return err
+        }
+    }
+    err := InitDataDir(path.Join(Blockchains, "refs"))
+    if err != nil{
+        return err
+    }
+    _, err = os.Create(path.Join(Blockchains, "HEAD"))
+    return err
+}
+
+func WriteJson(config interface{}, config_file string) error{
+	b, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "\t")
+    if err != nil{
+        return err
+    }
+	err = ioutil.WriteFile(config_file, out.Bytes(), 0600)
+    return err
+}
+
+func ChainIdFromName(name string) string{
+    b, err := ioutil.ReadFile(path.Join(Blockchains, "refs", name))
+    if err != nil{
+        return ""
+    }
+    return string(b)
+}
+
+func NewChainRef(name, chainId string) error{
+    p := path.Join(Blockchains, "refs", name)
+    _, err := os.Stat(p)
+    if err == nil{
+        return fmt.Errorf("Chain named %s already exists", name)
+    }
+    return ioutil.WriteFile(p, []byte(chainId), 0644)
+}
+
+
