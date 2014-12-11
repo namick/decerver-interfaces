0cc678257b3571c9c36a76e6f732b3bf64736cce
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2014-12-11 15:32:38 +0100

    add parse command to lmd glue

--- glue/legalmarkdown/lmdmodule.go
+++ glue/legalmarkdown/lmdmodule.go
@@ -1,18 +1,23 @@
 package legalmarkdown
 
 import (
-	// "github.com/eris-ltd/legalmarkdown"
+	"github.com/eris-ltd/legalmarkdown/lmd"
 	"github.com/eris-ltd/decerver-interfaces/core"
 	"github.com/eris-ltd/decerver-interfaces/events"
 )
 
 type LmdApi struct {
-	
 }
 
-// TODO api funcs.
-func (lmda * LmdApi) DoSomething(){
-	
+// pass two strings after reading a template file and an optional
+// params file. the params file can be an empty string if the
+// parameters are contained within the lmd template file that
+// is read into the contents string.
+//
+// the returned string will be a PDF which can be written or
+// displayed by an PDF reader.
+func (lmda *LmdApi) Parse(contents string, params string) string {
+	return lmd.RawMarkdownToPDF(contents, params)
 }
 
 // implements decerver-interface module
@@ -26,12 +31,10 @@ func NewLmdModule() *LmdModule {
 }
 
 func (mod *LmdModule) Register(fileIO core.FileIO, rm core.RuntimeManager, eReg events.EventRegistry) error {
-	// rm.RegisterApiObject("lmd", mod.api)
 	return nil
 }
 
 func (mod *LmdModule) Init() error {
-	
 	return nil
 }
 
@@ -39,19 +42,12 @@ func (mod *LmdModule) Start() error {
 	return nil
 }
 
-// TODO: UDP socket won't close
-// https://github.com/jbenet/go-ipfs/issues/389
 func (mod *LmdModule) Shutdown() error {
-	
 	return nil
 }
 
 func (mod *LmdModule) Restart() error {
-	err := mod.Shutdown()
-	if err != nil {
-		return nil
-	}
-	return mod.Start();
+	return nil
 }
 
 func (mod *LmdModule) SetProperty(name string, data interface{}) {
